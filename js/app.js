a9f11826a301134eeb9fe8e366ab9ef84021d4c4
Lara <lara.ej@gmail.com>
2018-07-26 20:34:41 +0100

    Making game launch

--- js/app.js
+++ js/app.js
@@ -24,12 +24,26 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function () {
+    this.sprite = 'images/char-boy.png';
 
+    this.x = 0;
+    this.y = 0;
+};
+
+Player.prototype.update = function(dt) {
+}
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+var allEnemies = [];
 
+var player = new Player();
 
 
 // This listens for key presses and sends the keys to your
